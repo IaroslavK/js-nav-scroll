d5bf2aae2c52e363049348cead99a3a1bb0c62ae
iaroslavk <iaroslavkhaidakin@gmail.com>
2023-10-09 18:14:12 +0300

    js-nav-scroll

---
+++ src/container/scroll/index.js
@@ -0,0 +1,32 @@
+window.scrollButton.onclick = () =>
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth',
+  })
+
+let isDisplay = false
+
+setInterval(() => {
+  if (
+    window.scrollY > window.innerHeight &&
+    isDisplay === false
+  ) {
+    isDisplay = true
+
+    window.scrollButton.style.display = 'flex'
+
+    return
+  }
+
+  if (
+    window.scrollY <= window.innerHeight &&
+    isDisplay === true
+  ) {
+    isDisplay = false
+
+    window.scrollButton.style.display = 'none'
+
+    return
+  }
+}, 500)
